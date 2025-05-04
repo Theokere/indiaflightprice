a8ee1a18bdf3926981474c2bf0f745edfbd53b14
Theokere <69910077+Theokere@users.noreply.github.com>
2025-05-05 03:00:44 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,6 @@ def main():
     if st.button('Submit'):        
         price_predict = loaded_model.predict(input_data)
         price_predict = format(price_predict[0],",")
-
         if source_city == destination_city:
             price_predict = 'Destination and Origin cannot be the same.'
         
